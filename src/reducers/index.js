50f4426ebf1241e2398db69381fd34e789c2dda3
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-13 12:32:34 +0200

    Configured redux form.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
   router: routerReducer,
+  form: formReducer,
   fortyTwo: () => 42
 })
